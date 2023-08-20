c261f61084dca58db87bc871007f60ee7bfb298f
Giovanni Moser <58645184+GiovanniMoser@users.noreply.github.com>
2023-08-20 00:13:55 +0000

    Update 2 files

--- main.py
+++ main.py
@@ -42,8 +42,8 @@ def load_LLM(openai_api_key):
     llm = OpenAI(temperature=.7, openai_api_key=openai_api_key)
     return llm
 
-st.set_page_config(page_title="Globalize Email", page_icon=":robot:")
-st.header("Globalize Text")
+st.set_page_config(page_title="Email Creator", page_icon=":robot:")
+st.header("Email Creator")
 
 col1, col2 = st.columns(2)
 
